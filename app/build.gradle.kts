06d69cf1093eb63b85efacb00ced79cee902259d
estermel <the.dev.story@gmail.com>
2023-11-30 08:52:04 +0700

    add hilt app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.github.bumptech.glide:glide:4.4.0")
+    ksp("com.github.bumptech.glide:compiler:4.4.0")
     implementation("com.google.dagger:hilt-android:2.48.1")
     ksp("com.google.dagger:hilt-android-compiler:2.48.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
